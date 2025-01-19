8b8594ba01d423d39e65760d49ceab56e19a173d
kyungsangYu <sangjx@naver.com>
2025-01-19 19:54:46 +0900

    .

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from chatbot.views import chatbot_page, get_answer  # views.py에서 함수 가져오기
-
-urlpatterns = [
-    path('', chatbot_page, name='chatbot_page'),       # 메인 페이지
-    path('get-answer', get_answer, name='get_answer')  # 챗봇 응답 처리 URL
-]
